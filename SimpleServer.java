8c574aeab6dbc4515fb9d5219b0da581eb5783c7
ColumbidaeKalla <sunilkalla4444@gmail.com>
2026-04-19 19:01:07 +0545

    Network Programming LAB-17 (Server)

---
+++ SimpleServer.java
@@ -0,0 +1,25 @@
+﻿/* Code to start a server */
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            System.out.println("Server is waiting...");
+
+            Socket socket = server.accept(); // wait for client
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("Hello from server!");
+
+            socket.close();
+            server.close();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
